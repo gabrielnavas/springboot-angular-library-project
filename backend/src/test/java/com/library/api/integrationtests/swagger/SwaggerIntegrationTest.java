5211b28e8c452978c07b83cbe1cf1c1e13965b2e
navas <gabrielnavas@email.com>
2023-10-05 15:18:57 -0300

    swagger integration test

---
+++ backend/src/test/java/com/library/api/integrationtests/swagger/SwaggerIntegrationTest.java
@@ -0,0 +1,32 @@
+package com.library.api.integrationtests.swagger;
+
+import com.library.api.TestConfigs;
+import com.library.api.integrationtests.testcontainers.AbstractIntegrationTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+
+import static io.restassured.RestAssured.given;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class SwaggerIntegrationTest extends AbstractIntegrationTest {
+
+    @Test
+    public void shouldDisplaySwaggerUiPage() {
+        String bodyContent = given()
+                .basePath("/swagger-ui/index.html")
+                .port(TestConfigs.SERVER_PORT)
+                .when()
+                .get()
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .extract()
+                .body()
+                .asString();
+
+        Assertions.assertTrue(bodyContent.contains("Swagger UI"));
+
+    }
+
+}
