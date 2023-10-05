7072dd634cb9719ad4f3d35a207a705c0921ef56
navas <gabrielnavas@email.com>
2023-10-05 01:04:37 -0300

    add handler all bad request exceptions

--- backend/src/main/java/com/library/api/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ backend/src/main/java/com/library/api/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.library.api.exceptions.handler;
 
+import com.library.api.exceptions.ObjectAlreadyExistsWith;
 import com.library.api.exceptions.http_responses.ExceptionResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,18 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex, WebRequest request) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(
+                        ExceptionResponse.builder()
+                                .timestamp(new Date())
+                                .details(request.getDescription(false))
+                                .message(ex.getMessage())
+                                .build()
+                );
+    }
+
+    @ExceptionHandler(ObjectAlreadyExistsWith.class)
+    public final ResponseEntity<ExceptionResponse> handleAllBadRequestsExceptions(ObjectAlreadyExistsWith ex, WebRequest request) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(
                         ExceptionResponse.builder()
                                 .timestamp(new Date())
