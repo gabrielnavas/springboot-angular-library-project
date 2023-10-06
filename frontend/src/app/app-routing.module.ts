243d4db8362c77ee1d57a5d3261dc854d30a6f5e
navas <gabrielnavas@email.com>
2023-10-05 22:08:35 -0300

    move publisher-component to components directory

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PublisherCompanyReadComponent } from './views/publisher-company/publisher-company-read/publisher-company-read.component';
+import { PublisherCompanyReadComponent } from './components/publisher-company/publisher-company-read/publisher-company-read.component';
 
 const routes: Routes = [{
   path: "publisher-company",
