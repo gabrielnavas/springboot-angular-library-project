800320963c4e3b6d517aba390bf2b61074f8ac72
navas <gabrielnavas@email.com>
2023-10-05 22:10:39 -0300

    move components inside publisher company

--- frontend/src/app/views/publisher-company/publisher-company.module.ts
+++ frontend/src/app/views/publisher-company/publisher-company.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PublisherCompanyReadComponent } from '../../components/publisher-company/publisher-company-read/publisher-company-read.component';
+import { PublisherCompanyReadComponent } from './components/publisher-company-read/publisher-company-read.component';
 
 
 
