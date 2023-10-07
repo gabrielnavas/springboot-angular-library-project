c5622f08bce4888634920e6dd50d70405220e695
navas <gabrielnavas@email.com>
2023-10-07 20:54:12 -0300

    add env variables

--- frontend/src/app/views/publisher-company/publisher-company.service.ts
+++ frontend/src/app/views/publisher-company/publisher-company.service.ts
@@ -3,22 +3,31 @@ import { PublisherCompany } from './publisher-company.model';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import { environment } from '../../../environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class PublisherCompanyService {
 
-  private baseUrl: string = "http://localhost:8080/api/v1/publishing-company"
+  private static CREATE_PUBLISHER_COMPANY_URL = 
+    `${environment.endpoints.baseUrl}/${environment.endpoints.publishingCompany.baseUrl}`
+
+  private static FIND_ALL_PUBLISHER_COMPANY_URL = 
+    `${environment.endpoints.baseUrl}/${environment.endpoints.publishingCompany.baseUrl}`
 
   constructor(
     private httpClient: HttpClient
   ) { }
 
   createPublisherCompany(publisherCompany: PublisherCompany): Observable<PublisherCompany> {
-    return this.httpClient.post<PublisherCompany>(this.baseUrl, publisherCompany)
+    return this.httpClient.post<PublisherCompany>(
+      PublisherCompanyService.CREATE_PUBLISHER_COMPANY_URL, 
+      publisherCompany
+    )
   }
 
   findAll(): Observable<PublisherCompany[]> {
-    return this.httpClient.get<PublisherCompany[]>(this.baseUrl)
+    return this.httpClient.get<PublisherCompany[]>(PublisherCompanyService.FIND_ALL_PUBLISHER_COMPANY_URL)
   }
 }
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  endpoints: {
+    baseUrl: "http://localhost:8080/api/v1",
+    publishingCompany: {
+      baseUrl: "publishing-company"
+    }
+  }
+};
