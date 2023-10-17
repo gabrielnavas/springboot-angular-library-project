5b51ea4076a21ed964902236144b4145c2cf92f9
navas <gabrielnavas@email.com>
2023-10-16 21:12:28 -0300

    add create table books on migrate flyway

---
+++ backend/src/main/resources/db/migration/V4__Create_Table_Books.sql
@@ -0,0 +1,16 @@
+CREATE TABLE public.books (
+	id uuid NOT NULL,
+	title varchar(255) NOT NULL,
+	isbn varchar(13) NOT NULL,
+	pages integer NOT NULL,
+	key_words varchar(1000),
+	year_publication date NOT NULL,
+	id_publishing_company uuid NOT NULL,
+	id_classification_books uuid NOT NULL,
+	id_author_books uuid NOT NULL,
+	foreign key (id_publishing_company) references publishing_company(id),
+	foreign key (id_classification_books) references classification_books(id),
+	foreign key (id_author_books) references author_books(id),
+	CONSTRAINT books_pk PRIMARY KEY (id),
+	CONSTRAINT book_title_unique_title UNIQUE (title)
+);
